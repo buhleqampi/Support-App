e9849b4bfe00fcfa2ebe32e2c6cbb3cc2f910bcb
buhle <buhle3857@gmail.com>
2024-02-09 15:18:54 +0200

    changes

--- support-app/app/api/db.ts
+++ support-app/app/api/db.ts
@@ -1,16 +1,16 @@
-import { Connection, createConnection } from 'mysql2/promise';
+// import { Connection, createConnection } from 'mysql2';
 
-const dbConfig = {
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'your_database',
-};
+// const dbConfig = {
+//   host: 'localhost',
+//   user: 'your_username',
+//   password: 'your_password',
+//   database: 'your_database',
+// };
 
-const connectToDatabase = async (): Promise<Connection> => {
-  const connection = await createConnection(dbConfig);
-  console.log('Connected to MySQL database');
-  return connection;
-};
+// const connectToDatabase = async (): Promise<Connection> => {
+//   const connection = await createConnection(dbConfig);
+//   console.log('Connected to MySQL database');
+//   return connection;
+// };
 
-export { connectToDatabase };
+// export { connectToDatabase };
